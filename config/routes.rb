334d7e09f20c83e1d5abce39682610ca8281db31
ppoveda <ppoveda@edicomgroup.com>
2011-05-23 12:37:20 +0200

    Done with 5.4

--- config/routes.rb
+++ config/routes.rb
@@ -34,10 +34,10 @@ ActionController::Routing::Routes.draw do |map|
   # map.root :controller => "welcome"
 
   # See how all your routes lay out with "rake routes"
-
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
+  map.signup  '/signup', :controller => 'users', :action => 'new'
   map.root    :controller => 'pages', :action => 'home'
 
   # Install the default routes as the lowest priority.
