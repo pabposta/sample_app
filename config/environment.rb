91364cf198018f8b250b0e466f047659ff0b0c52
ppoveda <ppoveda@edicomgroup.com>
2011-05-24 16:07:08 +0200

    Done with chapter7

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,8 @@ Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
-
+  config.gem 'gravatar_image_tag'
+  
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
