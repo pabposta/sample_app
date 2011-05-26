776a98d903a714284c10b190953112076accb0cc
Pablo <pabposta@gmail.com>
2011-05-26 22:58:02 +0200

    Done with 9.5

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,7 +15,7 @@ describe SessionsController do
       response.should be_success
     end
 	
-	it "should have the right title" do
+	  it "should have the right title" do
       get :new
       response.should have_tag("title", /sign in/i)
     end
@@ -43,7 +43,7 @@ describe SessionsController do
       end
     end
 	
-	describe "with valid email and password" do
+  	describe "with valid email and password" do
 
       before(:each) do
         @user = Factory(:user)
@@ -55,13 +55,25 @@ describe SessionsController do
 
       it "should sign the user in" do
         post :create, :session => @attr
-        # Fill in with tests for a signed-in user.
+        controller.current_user.should == @user
+        controller.should be_signed_in
       end
 
       it "should redirect to the user show page" do
         post :create, :session => @attr
         response.should redirect_to(user_path(@user))
       end
+    end
+  end
+  
+  describe "DELETE 'destroy'" do
+
+    it "should sign a user out" do
+      test_sign_in(Factory(:user))
+      controller.should be_signed_in
+      delete :destroy
+      controller.should_not be_signed_in
+      response.should redirect_to(root_path)
     end
   end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -85,6 +85,11 @@ describe UsersController do
         post :create, :user => @attr
         response.should render_template('new')
       end
+      
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
     end
 	
 	describe "success" do
