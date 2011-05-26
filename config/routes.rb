476a096eaff9134179bf3975366e2f967838e92d
ppoveda <ppoveda@edicomgroup.com>
2011-05-26 18:01:30 +0200

    In 9.3.2

--- config/routes.rb
+++ config/routes.rb
@@ -33,12 +33,15 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
   map.resources :users
+  map.resources :sessions, :only => [:new, :create, :destroy]
   
   # See how all your routes lay out with "rake routes"
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
   map.signup  '/signup', :controller => 'users', :action => 'new'
+  map.signin  '/signin',  :controller => 'sessions', :action => 'new'
+  map.signout '/signout', :controller => 'sessions', :action => 'destroy'
   map.root    :controller => 'pages', :action => 'home'
 
   # Install the default routes as the lowest priority.
