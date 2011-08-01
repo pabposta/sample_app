79524dffd1e4996816cb8768c2036ba936ddd117
Pablo <pabposta@gmail.com>
2011-08-01 21:47:49 +0200

    done with 12.4

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -21,12 +21,26 @@ class Micropost < ActiveRecord::Base
   
   default_scope :order => 'created_at DESC'
   
-  def self.from_users_followed_by(user)
-    followed_ids = user.following.map(&:id)
-    all(:conditions => ["user_id IN (#{followed_ids}) OR user_id = ?", user])
-  end
+  #def self.from_users_followed_by(user)
+  #  followed_ids = user.following.map(&:id)
+  #  all(:conditions => ["user_id IN (#{followed_ids}) OR user_id = ?", user])
+  #end
   
   def characters_left
     140
   end
+  
+  # Return microposts from the users being followed by the given user.
+  named_scope :from_users_followed_by, lambda { |user| followed_by(user) }
+
+  private
+
+    # Return an SQL condition for users followed by the given user.
+    # We include the user's own id as well.
+    def self.followed_by(user)
+      followed_ids = %(SELECT followed_id FROM relationships
+                       WHERE follower_id = :user_id)
+      { :conditions => ["user_id IN (#{followed_ids}) OR user_id = :user_id",
+                        { :user_id => user }] }
+    end
 end
